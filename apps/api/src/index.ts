f1d03f3fbfb6511e2a6ae8fbf6148d472580b496
dd <the.jesse.dev@gmail.com>
2025-04-17 14:19:17 -0700

    verified apis

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,2 @@
 export { api } from "../convex/_generated/api";
 export type { Doc, Id } from "../convex/_generated/dataModel";
-
-// TODO; define router function exports for frontend consumption; nvm, use internalMutations
